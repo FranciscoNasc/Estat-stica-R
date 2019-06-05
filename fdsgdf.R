5134e792b11ce50cdb10bbd24d9da595ebcad6b7
fmln <43381699+fmln@users.noreply.github.com>
2019-06-05 15:46:11 -0300

    sera

--- fdsgdf.R
+++ fdsgdf.R
@@ -21,20 +21,59 @@ median(got[,5])
 
 #questao 4
 
-#Fa�a uma fun��o que retorna apenas os nomes dos epis�dios que possuem notas maiores ou iguais a nove (9).
+#Faça uma função que retorna apenas os nomes dos episódios que possuem notas maiores ou iguais a nove (9).
 
-got[,2]
-
-fun = function(foda) {
-  x = foda[,3]
+fun = function(list) {
+  x = list[,3]
   u = 1
+  popeye = NULL
   for (i in x) {
-    if (foda[u,3] >= 9.0) {
-      triste = unlist(strsplit(as.character(foda[u,2]),"73")
-      print(triste[1])
+    if (list[u,3] >= 9.0) {
+      triste = as.character(list[u,2])
+      popeye = c(popeye,triste)
+                      
     }
     u = u +1
   }
+  return (popeye)
 }
 
 fun(got);
+
+#questao 5
+
+#Faça uma função que retorna o nome dos episódios com menor e maior notas, nessa ordem para cada uma das temporadas. 
+#Por fim, faça um dataframe com cada episódio encontrado com as colunas TÍTULO, NOTA, TEMPORADA ordenados de forma 
+#crescente por temporada (de 1 até 8).
+
+
+func = function(list) {
+  x = list[,1]
+  y = NULL
+  a = 15
+  b = 0
+  c = 0
+  for (i in list[,1]) {
+    if (i > c) {
+       c = i
+    }
+  }
+  for(j in 1:c){
+    u = 1;
+    for(k in list[,1]){
+      if(j==k){
+        if(list[u,3] >= b){
+          b = k
+        }else{
+          a = k
+        }
+      }else{
+        break;
+      }
+      u = u +1
+    }
+    print(a)
+    print(b)
+  }
+}
+func(got)
